8e7fc47ccb9102c8ff81f868a987a86d36c4692b
gabriel montibeller <gabriel.montibeller@gmail.com>
2019-07-21 04:56:46 -0300

    initial commit with project structure

---
+++ challenge-two/src/store/index.js
@@ -0,0 +1,20 @@
+import Vuex from 'vuex'
+
+const store = new Vuex.Store({
+  modules: {
+  },
+  state: {
+
+  },
+  actions: {
+
+  },
+  mutations: {
+
+  },
+  getters: {
+
+  }
+})
+
+export default store
